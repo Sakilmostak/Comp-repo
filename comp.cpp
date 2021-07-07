02e9a449bc79d56d1e6d40b4d32c1e21f45a5cb9
Sakil Mostak <skmahim71@gmail.com>
2021-07-07 22:17:05 +0530

    Number of subarray with sum zero

--- comp.cpp
+++ comp.cpp
@@ -2,52 +2,36 @@
 using namespace std;
 #define vi vector<int>
 
-struct Node{
-    int data;
-    Node* left;
-    Node* right;
-    Node(int val){
-        data= val;
-        right= NULL;
-        left= NULL;
-    }
-};
-
-void getVerticalOrder(Node* root, int hdis, map<int, vi> &m){
-    if(root==NULL){
-        return;
-    }
-
-    m[hdis].push_back(root->data);
-    getVerticalOrder(root->left, hdis-1, m);
-    getVerticalOrder(root->right, hdis+1, m);
-}
-
 int main(){
-    Node* root= new Node(10);
-    root->left= new Node(7);
-    root->right= new Node(4);
-    root->left->left= new Node(3);
-    root->left->right= new Node(11);
-    root->right->left= new Node(14);
-    root->right->right= new Node(6);
-    
+    int n;
+    cin>>n;
 
-    map<int,vi> m;
-    int hdistance= 0;
-
-    getVerticalOrder(root, hdistance, m);
-    
-    map<int,vi> :: iterator it;
-
-    for(it=m.begin();it!=m.end();it++){
-        for(int i=0;i<(it->second).size();i++){
-
-            cout<<(it->second)[i]<<" ";
-
-        }
-        cout<<endl;
+    vi a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
     }
 
-    return 0;
+    int preSum=0;
+    map<int,int> count;
+
+    for(int i=0;i<n;i++){
+        preSum+= a[i];
+        count[preSum]++;
+
+    }
+
+    int ans=0;
+
+    map<int,int> :: iterator it;
+
+    for(it= count.begin();it!= count.end();it++){
+        int c= it->second;
+        ans+= (c*(c-1))/2;
+
+        if(it->first==0){
+            ans+=it->second;
+        }
+    }
+
+    cout<<ans<<endl;
 }
