7b667efa4ddfa6d144ee1080d50124d170863da0
Sakil Mostak <skmahim71@gmail.com>
2021-07-19 21:34:13 +0530

    MAXDIFF problem on Codechef

--- comp.cpp
+++ comp.cpp
@@ -1,73 +1,50 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-
-bool isValid(vector<vector<int>> &board, int row, int column, int key){
-    for(int i=0;i<9;i++){
-        if(board[row][i]==key){
-            return false;
-        }
-
-        if(board[i][column]==key){
-            return false;
-        }
-
-        if(board[3*(row/3)+(i/3)][3*(column/3)+(i%3)]==key){
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool solveSudoku(vector<vector<int>> &board){
-    for (int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            if(board[i][j]==0){
-                for(int k=1;k<=9;k++){
-                    if(isValid(board,i,j,k)){
-                        board[i][j]=k;
-
-                        if(solveSudoku(board)){
-                            return true;
-                        }
-                        else{
-                            board[i][j]=0;
-                            return false;
-                        }
-                    }
-                }
-                return false;
-            }
-        }
-    }
-    return true;
-}
 
 
 int main(){
-
-    vector<vector<int>> board 
-       { { 3, 1, 6, 5, 7, 8, 4, 9, 2 },
-         { 5, 2, 9, 1, 3, 4, 7, 6, 8 },
-         { 4, 8, 7, 6, 2, 9, 5, 3, 1 },
-         { 2, 6, 3, 0, 1, 5, 9, 8, 7 },
-         { 9, 7, 4, 8, 6, 0, 1, 2, 5 },
-         { 8, 5, 1, 7, 9, 2, 6, 4, 3 },
-         { 1, 3, 8, 0, 4, 7, 2, 0, 6 },
-         { 6, 9, 2, 3, 5, 1, 8, 7, 4 },
-         { 7, 4, 5, 0, 8, 6, 3, 1, 0 } };
-
-    if(solveSudoku(board)){
-
-        for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            cout<<board[i][j]<<" ";
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
         }
-        cout<<endl;
+
+        sort(arr.begin(), arr.end());
+
+        int kw1=0,aw1=0;
+
+        for(int i=0;i<n;i++){
+            if(i<k){
+                kw1+=arr[i];
+            }
+            else{
+                aw1+=arr[i];
+            }
         }
+
+        int kw2=0, aw2=0;
+
+        for(int i=n-1;i>=0;i++){
+            if(k){
+                kw2+=arr[i];
+                k--;
+            }
+            else{
+                aw2+=arr[i];
+            }
+        }
+
+        int ans= max((aw1-kw1),(kw2-aw2));
+
+        cout<<ans<<endl;
+
+        
     }
-    
 
     return 0;
-    
 }
