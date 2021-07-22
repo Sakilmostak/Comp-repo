ebdf98b75a7605315bee63813d7ef22a05a3617a
Sakil Mostak <skmahim71@gmail.com>
2021-07-22 21:26:12 +0530

    ERROR problem from Codechef

--- comp.cpp
+++ comp.cpp
@@ -7,44 +7,35 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n,k;
-        cin>>n>>k;
-        vector<int> arr(n);
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
+        string s;
+        cin>>s;
+        
+        string ch=s.substr(0,3);
+        bool see=true;
+        
+        if(ch=="101" || ch=="010"){
+            cout<<"Good"<<endl;
+            see=false;
         }
-
-        sort(arr.begin(), arr.end());
-
-        int kw1=0,aw1=0;
-
-        for(int i=0;i<n;i++){
-            if(i<k){
-                kw1+=arr[i];
+        else{
+            for(int i=3;i<s.size();i++){
+                
+                ch.push_back(s[i]);
+                ch.erase(ch.begin());
+                if(ch=="101" || ch=="010"){
+                    cout<<"Good"<<endl;
+                    see=false;
+                    break;
+                }
             }
-            else{
-                aw1+=arr[i];
-            }
+            
         }
-
-        int kw2=0, aw2=0;
-
-        for(int i=n-1;i>=0;i++){
-            if(k){
-                kw2+=arr[i];
-                k--;
-            }
-            else{
-                aw2+=arr[i];
-            }
+        
+        if(see){
+            cout<<"Bad"<<endl;
         }
-
-        int ans= max((aw1-kw1),(kw2-aw2));
-
-        cout<<ans<<endl;
+        
 
         
     }
-
-    return 0;
 }
