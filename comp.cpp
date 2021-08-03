05be85ed22e1b941bff60f15423a7283e725c1d5
Sakil Mostak <skmahim71@gmail.com>
2021-08-03 12:20:11 +0530

    Cheff and his triangles(codechef)

--- comp.cpp
+++ comp.cpp
@@ -4,26 +4,31 @@
 using namespace std;
 
 int main(){
-    int n,d;
-    cin>>n >>d;
-    vector<int> ar(n);
-    for(int i=0;i<n;i++){
-        cin>>ar[i];
-    }
+    int n;
+    cin>>n;
 
-    sort(ar.begin(), ar.end());
+    int count=0;
 
-    int i=0, count=0;
+    while(n--){
+        int x1,y1,x2,y2,x3,y3;
+        cin>>x1>>y1>>x2>>y2>>x3>>y3;
 
-    while(i<n-1){
-        if(abs(ar[i]-ar[i+1])<=d){
+        int s1= pow(x1-x2,2)+pow(y1-y2,2);
+        int s2= pow(x1-x3,2)+pow(y1-y3,2);
+        int s3= pow(x3-x2,2)+pow(y3-y2,2);
+
+        if(s1+s2==s3){
             count++;
-            i+=2;
         }
-        else{
-            i++;
+        else if(s1+s3==s2){
+            count++;
+        }
+        else if(s2+s3==s1){
+            count++;
         }
     }
 
     cout<<count<<endl;
+
+
 }
