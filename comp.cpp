6faa77ff849f9324620b845dfd73f7ff87b6d121
Sakil Mostak <skmahim71@gmail.com>
2021-08-03 13:05:11 +0530

    RESQ (codechef)

--- comp.cpp
+++ comp.cpp
@@ -4,31 +4,26 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-
-    int count=0;
-
-    while(n--){
-        int x1,y1,x2,y2,x3,y3;
-        cin>>x1>>y1>>x2>>y2>>x3>>y3;
-
-        int s1= pow(x1-x2,2)+pow(y1-y2,2);
-        int s2= pow(x1-x3,2)+pow(y1-y3,2);
-        int s3= pow(x3-x2,2)+pow(y3-y2,2);
-
-        if(s1+s2==s3){
-            count++;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> div;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                div.push_back(i);
+            }
         }
-        else if(s1+s3==s2){
-            count++;
+        
+        int s= div.size();
+        
+        int ans=INT_MAX;
+        for(int i=0;i<s;i++){
+            ans= min(abs(div[i]-div[s-1-i]),ans);
         }
-        else if(s2+s3==s1){
-            count++;
-        }
+        
+        cout<<ans<<endl;
+
     }
-
-    cout<<count<<endl;
-
-
 }
