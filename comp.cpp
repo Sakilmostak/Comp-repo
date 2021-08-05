71b588e9530b4c9fa32707700991f2ef320fe704
Sakil Mostak <skmahim71@gmail.com>
2021-08-05 09:42:19 +0530

    OJUMPS

--- comp.cpp
+++ comp.cpp
@@ -4,26 +4,12 @@
 using namespace std;
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> div;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                div.push_back(i);
-            }
-        }
-        
-        int s= div.size();
-        
-        int ans=INT_MAX;
-        for(int i=0;i<s;i++){
-            ans= min(abs(div[i]-div[s-1-i]),ans);
-        }
-        
-        cout<<ans<<endl;
-
+    ll a;
+    cin>>a;
+    if(a%3==0 || a%6==1){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
     }
 }
