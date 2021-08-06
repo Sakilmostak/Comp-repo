4703e24244106b4b9ed100b9fe762cdd5358bed7
Sakil Mostak <skmahim71@gmail.com>
2021-08-06 19:41:40 +0530

    August long q2

--- comp.cpp
+++ comp.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-#define ll int long long
 
 using namespace std;
 
@@ -7,14 +6,33 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int l,r;
-        cin>>l>>r;
+        map<int,int> m;
+        for(int i=0;i<4;i++){
+            int x;
+            cin>>x;
+            m[x]++;
+        }
+
+        map<int,int> :: iterator it;
         int count=0;
-        for(int i=l;i<=r;i++){
-            if(i%10==2 || i%10==3 || i%10==9){
-                count++;
+
+        for(it=m.begin();it!=m.end();it++){
+            count++;
+        }
+        if(count==1){
+            cout<<"0"<<endl;
+        }
+        else if(count==2){
+            if(m.begin()->second==2){
+                cout<<"2"<<endl;
+            }
+            else{
+                cout<<"1"<<endl;
             }
         }
-        cout<<count<<endl;
+        else if(count==3 || count==4){
+            cout<<"2"<<endl;
+        }
+
     }
 }
