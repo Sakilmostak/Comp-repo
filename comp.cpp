666a781e0ba8d2031ebe12e4168dee3e5c77a368
Sakil Mostak <skmahim71@gmail.com>
2021-08-10 10:29:31 +0530

    SPCTRIPS

--- comp.cpp
+++ comp.cpp
@@ -2,37 +2,44 @@
 
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        map<int,int> m;
-        for(int i=0;i<4;i++){
-            int x;
-            cin>>x;
-            m[x]++;
-        }
+void checkTriplet(int a, int b,int c,int n, int* count){
+	if(a>n){
+		checkTriplet(1,b+c,c,n,count);
+		return;
+	}
+	if(b>n){
+		return;
+	}
 
-        map<int,int> :: iterator it;
-        int count=0;
+	if(b%c==0){
+		if(a%b==c){
+			*count+=1;
+		}
+	}
 
-        for(it=m.begin();it!=m.end();it++){
-            count++;
-        }
-        if(count==1){
-            cout<<"0"<<endl;
-        }
-        else if(count==2){
-            if(m.begin()->second==2){
-                cout<<"2"<<endl;
-            }
-            else{
-                cout<<"1"<<endl;
-            }
-        }
-        else if(count==3 || count==4){
-            cout<<"2"<<endl;
-        }
+	checkTriplet(a+1,b,c,n,count);
 
-    }
+	return;
 }
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+	cin>>n;
+	int count=0;
+	for(int c=1;c<=n/2;c++){
+		checkTriplet(1,c,c,n,&count);
+	}
+
+	cout<<count<<endl;
+	}
+	
+	return 0;
+}
+	
--- practice.cpp
+++ practice.cpp
@@ -2,57 +2,41 @@
 
 using namespace std;
 
-int inTime[1000];
-int outTime[1000];
+void checkTriplet(int a, int b,int c,int n, int* count){
+	if(a==0){
+		checkTriplet(n,b-1,c,n,count);
+		return;
+	}
+	if(b==1){
+		return;
+	}
 
-int inT=0,outT=0;
-
-int dfs(int s, vector<vector<int>> & adjList, vector<bool> & visited){
-	visited[s]=true;
-	inTime[s]=inT;
-	inT++;
-
-	for(int adjNode: adjList[s]){
-		if(!visited[adjNode]){
-			dfs(adjNode, adjList, visited);
+	if(b%c==0){
+		if(a%b==c){
+			*count+=1;
 		}
 	}
-	outTime[s]=outT;
-	outT++;
+
+	checkTriplet(a-1,b,c,n,count);
+
+	return;
 }
+
 
 int main(){
-	int n; // no of vertices
-	int m; // no of edges
-
-	cin>>n>>m;
-
-	vector<vector<int>> adjList(n);  // Adjacency List representation of Graph
-
-	for(int i=0;i<m;i++){
-		int from, to;
-		cin>>from>>to;
-		adjList[from].push_back(to);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+	cin>>n;
+	int count=0;
+	for(int c=1;c<=n/2;c++){
+		checkTriplet(n,n,c,n,&count);
 	}
 
-
-	const int INF = 100000000;
-
-	vector<bool> visited(n, false);
-	
-	// to find the number of connected components
-	int conn_comp_count=0;
-	for(int nd=0;nd<n;nd++){
-		if(!visited[nd]){
-			//run a BFS
-			conn_comp_count+=1;
-			int node_count= run_bfs(nd,adjList,visited);
-			cout<<"We ran BFS from "<<nd<<", node-count = "<<node_count<<endl;
-		}
+	cout<<count<<endl;
 	}
-
-	cout<<"\n Total "<<conn_comp_count<<" Connected Components"<<endl;
 	
-
 	return 0;
 }
+	
