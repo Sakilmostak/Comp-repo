d8252f7dc8f912f17087f5ff2ee9526009ff31ef
Sakil Mostak <skmahim71@gmail.com>
2021-09-02 09:50:25 +0530

    Pair Sum to Zero(CodingNinjas)

--- comp.cpp
+++ comp.cpp
@@ -2,71 +2,37 @@
 
 using namespace std;
 
-int run_bfs(int s, vector<vector<int>> & adjList, vector<bool> & visited){
-	queue<int> Q;
-	Q.push(s);
-	visited[s]= true;
-	int node_count=0;
-	while(!Q.empty()){
-		s=Q.front();
-		Q.pop();
-		node_count+=1;
-
-		for(int adjNode: adjList[s]){
-			if(!visited[adjNode]){
-				visited[adjNode]=true;
-				Q.push(adjNode);
-			}
-		}
-	}
-
-	return node_count;
-}
-
 int main(){
-	int t;
+    int t;
     cin>>t;
     while(t--){
-        int n; // no of vertices
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
 
-	cin>>n;
+        int bsize=arr[0];
+        int step=0;
+        int bstep=0;
 
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-
-	vector<vector<int>> adjList(n);  // Adjacency List representation of Graph
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(j!=i){
-                if(j-i!=arr[j]-arr[i]){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+        for(int i=1;i<n;i++){
+            if(arr[i]<=arr[i-1]){
+                step++;
+            }
+            else{
+                if(arr[i]>bsize){
+                    cout<<"-1"<<endl;
+                }
+                else{
+                    bsize=arr[i];
+                    bstep++;
+                    step=0;
                 }
             }
-
         }
+
+        cout<<bstep+step<<endl;
     }
-
-	const int INF = 100000000;
-
-	vector<bool> visited(n, false);
-	
-	// to find the number of connected components
-	int conn_comp_count=0;
-	for(int nd=0;nd<n;nd++){
-		if(!visited[nd]){
-			//run a BFS
-			conn_comp_count+=1;
-			int node_count= run_bfs(nd,adjList,visited);
-		}
-	}
-
-	cout<<conn_comp_count<<endl;
-	
-
-    }
-	return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -1,37 +1,62 @@
-#include<iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-class upper{
-protected:
-    int data;
+int pairSum(int *arr,int n){
+    sort(arr,arr+n);
 
-public:
-    upper(int x){
-        data=x;   
+    int start=0;
+    int end=n-1;
+    int a=0,b=0;
+    int count=0;
+    while(start<end){
+        if((arr[start]+arr[end])==0){
+            int para1= arr[start];
+            int para2= arr[end];
+            while(arr[start]==para1 && start<n){
+                a++;
+                start++;
+            }
+            while(arr[end]==para2 && end>0){
+                b++;
+                end--;
+            }
+            if(para1==0){
+                count=count + ((a*(a-1))/2);
+            }
+            else if(para2==0){
+                count= count+ ((b*(b-1))/2);
+            }
+            else{
+                count= count + (a*b);
+            }
+            a=0;
+            b=0;
+        }
+        while((arr[start]+arr[end])>0){
+            end--;
+        }
+        while((arr[start]+arr[end])<0){
+            start++;
+        }
     }
 
-    void print1(){
-        cout<<data<<endl;
-    }
-};
-
-class lower: public upper{
-private:
-    int data1;
-public:
-    lower(){
-        data1=data;
-    }
-    
-    void print2(){
-        cout<<data1<<endl;
-    }
-};
-
-int main(){
-    upper made(50);
-    nextmade.print2();
-
-    return 0;
+    return count;
 }
 
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << pairSum(arr, n);
+
+    delete[] arr;
+}
