5bc4f36e6c809d22b874963a42d0618a3f2ccd45
Sakil Mostak <skmahim71@gmail.com>
2021-09-05 14:08:15 +0530

    PALINT

--- comp.cpp
+++ comp.cpp
@@ -3,41 +3,69 @@
 
 using namespace std;
 
+bool compare(pair<int,int> p1, pair<int,int> p2){
+    if(p1.first!=p2.first){
+        return p1.first>p2.first;
+    }
+
+    return p1.second<p2.second;
+}
+
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
-        cin>>n;
-        ll arr[n];
+        int n,x;
+        cin>>n>>x;
+        vector<ll> arr(n);
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
 
-        int encnt,oncnt,eacnt=0,oacnt=0;
+        if(x==0){
+            map<ll,int> mapy;
 
-        encnt=n/2;
-        if(n%2==0){
-            oncnt=n/2;
+            for(int i=0;i<n;i++){
+                mapy[arr[i]]++;
+            }
+
+            map<ll,int> :: iterator ita;
+            int maxfreq=0;
+
+            for(ita= mapy.begin();ita!=mapy.end();ita++){
+                if(ita->second>maxfreq){
+                    maxfreq=ita->second;
+                }
+            }
+
+            cout<<maxfreq<<" "<<"0"<<endl;
         }
         else{
-            oncnt=(n/2)+1;
-        }
+
+        map<ll,int> mp;
+        map<ll,int> freq;
+
 
         for(int i=0;i<n;i++){
-            if(arr[i]%2==0){
-                eacnt++;
-            }
-            else{
-                oacnt++;
-            }
+            mp[arr[i]]++;
+            mp[arr[i]^x]++;
+            freq[arr[i]^x]++;
         }
 
-        int ans1= min(encnt,oacnt);
-        int ans2= min(eacnt,oncnt);
-        int ans= ans1+ans2;
 
-        cout<<ans<<endl;
-        
+        map<ll,int> :: iterator it;
+        vector<pair<int,int>> ans;
+
+        for(it=mp.begin();it!=mp.end();it++){
+            pair<int,int> temp;
+            temp.first=it->second;
+            temp.second=freq[it->first];
+            ans.push_back(temp);
+        }
+
+        sort(ans.begin(),ans.end(),compare);
+
+        cout<<ans[0].first<<" "<<ans[0].second<<endl;
+        }
     }
 }
