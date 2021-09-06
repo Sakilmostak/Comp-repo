3126277f203d1480b592776a933b39df9f186a85
Sakil Mostak <skmahim71@gmail.com>
2021-09-06 16:40:58 +0530

    MINDIGSUM

--- comp.cpp
+++ comp.cpp
@@ -3,69 +3,39 @@
 
 using namespace std;
 
-bool compare(pair<int,int> p1, pair<int,int> p2){
-    if(p1.first!=p2.first){
-        return p1.first>p2.first;
-    }
-
-    return p1.second<p2.second;
-}
-
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int n,x;
-        cin>>n>>x;
-        vector<ll> arr(n);
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-        }
-
-        if(x==0){
-            map<ll,int> mapy;
-
-            for(int i=0;i<n;i++){
-                mapy[arr[i]]++;
-            }
-
-            map<ll,int> :: iterator ita;
-            int maxfreq=0;
-
-            for(ita= mapy.begin();ita!=mapy.end();ita++){
-                if(ita->second>maxfreq){
-                    maxfreq=ita->second;
-                }
-            }
-
-            cout<<maxfreq<<" "<<"0"<<endl;
+        ll n,l,r;
+        cin>>n>>l>>r;
+        int base;
+        if(r>=n && n>=l){
+            base=n;
         }
         else{
+            int ans=INT_MAX;
+            for(ll i=l;i<=r;i++){
+                if(i>=n){
+                    base=i;
+                }
+                else{
+                    ll p=n;
+                    int sum=0;
+                    while(p){
+                        sum+=p%i;
+                        p=p/i;
+                    }
 
-        map<ll,int> mp;
-        map<ll,int> freq;
-
-
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
-            mp[arr[i]^x]++;
-            freq[arr[i]^x]++;
+                    if(sum<ans){
+                        ans=sum;
+                        base=i;
+                    }
+                }
+            
+            }
         }
-
-
-        map<ll,int> :: iterator it;
-        vector<pair<int,int>> ans;
-
-        for(it=mp.begin();it!=mp.end();it++){
-            pair<int,int> temp;
-            temp.first=it->second;
-            temp.second=freq[it->first];
-            ans.push_back(temp);
-        }
-
-        sort(ans.begin(),ans.end(),compare);
-
-        cout<<ans[0].first<<" "<<ans[0].second<<endl;
-        }
+        
+        cout<<base<<endl;
     }
 }
