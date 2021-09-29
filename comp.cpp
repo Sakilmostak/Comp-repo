23dcd72ef12bbd3d10bb9f99ab9cfd20a0e3d47a
Sakil Mostak <skmahim71@gmail.com>
2021-09-29 20:41:26 +0530

    Light Up the Bulbs(Coding Ninjas)

--- comp.cpp
+++ comp.cpp
@@ -1,44 +1,44 @@
 #include<bits/stdc++.h>
+#define ll long long
 
 using namespace std;
-
-bool descending(pair<int,int> p1, pair<int,int> p2){
-    return p1.first>p2.first;
-}
 
 int main(){
     int n;
     cin>>n;
-    vector<pair<int,int>> rect;
+    ll xcost,ycost;
+    cin>>xcost>>ycost;
+    string bulb;
+    cin>>bulb;
+    
+    int count=0;
+    bool check=true;
     for(int i=0;i<n;i++){
-        int x,y;
-        cin>>x>>y;
-        rect.push_back(make_pair(x/2,y));
-    }
-
-    sort(rect.begin(),rect.end(),descending);
-    long long area=0;
-    int width=rect[0].second;
-
-    for(int i=0;i<n-1;i++){
-        int xwidth= rect[i].first-rect[i+1].first;
-        if(rect[i].second>width){
-            width=rect[i].second;
+        if(bulb[i]=='0'){
+            check=false;
         }
-
-        area+= (xwidth*width);
+    }
+    for(int i=0;i<n-1;i++){
+        if(bulb[i]=='0' && bulb[i+1]=='1'){
+            count++;
+        }
+    }
+    
+    if(check){
+        cout<<"0"<<endl;
+    }
+    else{
+        int xcount=count;
+        int ycount=count+1;
+        if(bulb[n-1]=='1'){
+            xcount--;
+            ycount--;
+        }
+        
+        ll ans1=(xcount*xcost)+ycost;
+        ll ans2=(ycount*ycost);
+        cout<<min(ans1,ans2)<<endl;
     }
 
-    if(rect[n-1].second>width){
-        width=rect[n-1].second;
-    }
-
-    area+= (width*rect[n-1].first);
-    area*=2;
-
-    cout<<area<<endl;
-
-    /* for(int i=0;i<n;i++){
-        cout<<rect[i].first<<" "<<rect[i].second<<endl;
-    } */
+    
 }
