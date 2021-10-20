e952fa1775ce63c5d0a9656efd9e24bda0a5d4cd
Sakil Mostak <skmahim71@gmail.com>
2021-10-20 12:37:13 +0530

    Magic Grid Problem(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,56 +3,45 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
-ll tNumExchange(int  arr[],int n, int value, ll** check){
-	if(n<=0){
-		return 0;
-	}
-	if(value==0){
-		return 1;
-	}
-    if(value<0){
-		return 0;
-	}
-
-	if(check[value][n]!=-1){
-		return check[value][n];
-	}
-
-	ll first= tNumExchange(arr,n,value-arr[0],check)%mod;
-	ll second= tNumExchange(arr+1,n-1,value,check)%mod;
-	ll total= (first+second)%mod;
-	if(check[value][n]==-1){
-        check[value][n]=total;
-    }
-
-	return total;
-}
-
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n;
-		cin>>n;
-		int arr[n];
-		for(int i=0;i<n;i++){
-			cin>>arr[i];
-		}
-		int value;
-		cin>>value;
-		ll **check= new ll*[value+1];
-
-		for(int i=0;i<=value;i++){
-			check[i]= new ll[n+1];
-		}
-
-		for(int i=0;i<=value;i++){
-			for(int j=0;j<=n;j++){
-				check[i][j]=-1;
+		int r,c;
+		cin>>r>>c;
+		int maze[r][c];
+		for(int i=0;i<r;i++){
+			for(int j=0;j<c;j++){
+				cin>>maze[i][j];
 			}
 		}
-		ll ans= tNumExchange(arr,n,value,check);
-        cout<<ans<<endl;
-		
+
+		int dp[r][c];
+		dp[r-1][c-1]=1;
+		//dp[r-2][c-1]=1;
+		//dp[r-1][c-2]=1;
+		for(int i=r-2;i>=0;i--){
+			dp[i][c-1]=dp[i+1][c-1]-maze[i][c-1];
+			if(dp[i][c-1]<=0){
+				dp[i][c-1]=1;
+			}
+		}
+		for(int j=c-2;j>=0;j--){
+			dp[r-1][j]=dp[r-1][j+1]-maze[r-1][j];
+			if(dp[r-1][j]<=0){
+				dp[r-1][j]=1;
+			}
+		}
+
+		for(int i=r-2;i>=0;i--){
+			for(int j=c-2;j>=0;j--){
+				dp[i][j]=min(dp[i+1][j],dp[i][j+1])-maze[i][j];
+				if(dp[i][j]<=0){
+					dp[i][j]=1;
+				}
+			}
+		}
+
+		cout<<dp[0][0]<<endl;
 	}
 }
