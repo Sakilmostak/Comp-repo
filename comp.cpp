4d06d21881febcb550ebaff9441a2b6eb4a04fc3
Sakil Mostak <skmahim71@gmail.com>
2021-10-20 20:03:16 +0530

    Loot Houses(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -4,44 +4,29 @@ using namespace std;
 #define ll long long
 
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int r,c;
-		cin>>r>>c;
-		int maze[r][c];
-		for(int i=0;i<r;i++){
-			for(int j=0;j<c;j++){
-				cin>>maze[i][j];
-			}
-		}
-
-		int dp[r][c];
-		dp[r-1][c-1]=1;
-		//dp[r-2][c-1]=1;
-		//dp[r-1][c-2]=1;
-		for(int i=r-2;i>=0;i--){
-			dp[i][c-1]=dp[i+1][c-1]-maze[i][c-1];
-			if(dp[i][c-1]<=0){
-				dp[i][c-1]=1;
-			}
-		}
-		for(int j=c-2;j>=0;j--){
-			dp[r-1][j]=dp[r-1][j+1]-maze[r-1][j];
-			if(dp[r-1][j]<=0){
-				dp[r-1][j]=1;
-			}
-		}
-
-		for(int i=r-2;i>=0;i--){
-			for(int j=c-2;j>=0;j--){
-				dp[i][j]=min(dp[i+1][j],dp[i][j+1])-maze[i][j];
-				if(dp[i][j]<=0){
-					dp[i][j]=1;
-				}
-			}
-		}
-
-		cout<<dp[0][0]<<endl;
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
 	}
+
+	ll dp[n];
+	dp[0]=arr[0];
+	dp[1]=arr[1];
+	ll maxbonus= arr[0];
+	for(int i=2;i<n;i++){
+		maxbonus=max(maxbonus,dp[i-2]);
+		dp[i]=maxbonus+ arr[i];
+	}
+
+	ll ans=0;
+
+	for(int i=0;i<n;i++){
+		ans=max(ans,dp[i]);
+	}
+
+	cout<<ans<<endl;
+
+	return 0;
 }
