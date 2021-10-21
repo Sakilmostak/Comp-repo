12bc8b7be34e60f142ee72006403c5086c9ab604
Sakil Mostak <skmahim71@gmail.com>
2021-10-21 10:36:53 +0530

    Minimum Count(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,30 +3,48 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
+ll findMinCount(int arr[], int size, int num, int output[]){
+	if(num==0){
+		return 0;
+	}
+	if(num<0){
+		return INT_MAX;
+	}
+	if(size==0){
+		return INT_MAX;
+	}
+
+	int tosub=pow(arr[0],2);
+	if(output[num]!=-1){
+		return output[num];
+	}
+	ll first= findMinCount(arr,size,num-tosub,output);
+	ll second=  findMinCount(arr+1,size-1,num,output);
+	ll count= min(first+1,second);
+	output[num]=count;
+	//cout<<count<<" "<<num<<endl;
+	return  count;
+}
+
 int main(){
-	int n;
-	cin>>n;
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int req= sqrt(n);
+		int arr[req];
+		for(int i=0;i<req;i++){
+			arr[i]=i+1;
+		}
+
+		int output[n+1];
+		for(int i=0;i<=n;i++){
+			output[i]=-1;
+		}
+
+		ll ans= findMinCount(arr,req,n,output);
+		cout<<ans<<endl;
+
 	}
-
-	ll dp[n];
-	dp[0]=arr[0];
-	dp[1]=arr[1];
-	ll maxbonus= arr[0];
-	for(int i=2;i<n;i++){
-		maxbonus=max(maxbonus,dp[i-2]);
-		dp[i]=maxbonus+ arr[i];
-	}
-
-	ll ans=0;
-
-	for(int i=0;i<n;i++){
-		ans=max(ans,dp[i]);
-	}
-
-	cout<<ans<<endl;
-
-	return 0;
 }
