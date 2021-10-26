68f2cf1db22354276fa6b5b13e2e32e321e41e2a
Sakil Mostak <skmahim71@gmail.com>
2021-10-26 21:00:20 +0530

    Knapsack(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,45 +3,62 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
+struct items{
+	int wt;
+	int value;
+};
+
+int findMaxValue(items it[],int n, int knapwt, int** dp){
+	if(n==0){
+		return 0;
+	}
+
+	if(dp[n][knapwt]!=-1){
+		return dp[n][knapwt];
+	}
+
+	int first=0;
+
+	if(knapwt-it[0].wt>=0){
+		first= it[0].value + findMaxValue(it+1,n-1,knapwt-it[0].wt,dp);
+	}
+
+	int second= findMaxValue(it+1,n-1,knapwt,dp);
+
+	int ans= max(first,second);
+	dp[n][knapwt]=ans;
+	return ans;	
+}
+
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		char s1[101];
-		char s2[101];
-		cin>>s1;
-		cin>>s2;
-		int m= strlen(s1);
-		int n= strlen(s2);
-
-		int** dp= new int*[m+1];
-		for(int i=0;i<=m;i++){
-			dp[i]= new int[n+1];
+	
+		int n;
+		cin>>n;
+		items it[n];
+		for(int i=0;i<n;i++){
+			cin>>it[i].wt;
+		}
+		for(int i=0;i<n;i++){
+			cin>>it[i].value;
 		}
 
-		for(int i=0;i<=m;i++){
-			dp[i][0]=i;
-		}
+		int knapwt;
+		cin>>knapwt;
+		int** dp= new int*[n+1];
 		for(int i=0;i<=n;i++){
-			dp[0][i]=i;
-		}
-
-		for(int i=1;i<=m;i++){
-			for(int j=1;j<=n;j++){
-				if(s1[n-i]==s2[n-i]){
-					dp[i][j]=dp[i-1][j-1];
-				}
-				else{
-					dp[i][j]=min(1+dp[i-1][j-1],min(1+dp[i-1][j],1+dp[i][j-1]));
-				}
+			dp[i]= new int[knapwt+1];
+			for(int j=0;j<=knapwt;j++){
+				dp[i][j]=-1;
 			}
 		}
 
-		int ans= dp[m][n];
-		for(int i=0;i<=m;i++){
+		int ans= findMaxValue(it,n,knapwt,dp);
+
+		for(int i=0;i<=n;i++){
 			delete [] dp[i];
 		}
 		delete [] dp;
+
 		cout<<ans<<endl;
-	}
+		
 }
