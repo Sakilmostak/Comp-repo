9441631aae4640c3bda15a8a7241631f0e763422
Sakil Mostak <skmahim71@gmail.com>
2021-10-26 10:40:21 +0530

    Angry Children(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -7,53 +7,37 @@ int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n,m;
-		cin>>n>>m;
-		int arr[n+1][m+1];
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=m;j++){
-				cin>>arr[i][j];
-			}
+		int n,k;
+		cin>>n>>k;
+		ll arr[n];
+		for(int i=0;i<n;i++){
+			cin>>arr[i];
 		}
 
-		int dp[n+1][m+1];
-		for(int i=1;i<=m;i++){
-			dp[1][i]=1;
+		sort(arr,arr+n);
+
+		vector<ll> target;
+		target.push_back(0);
+		ll sum=arr[0];
+
+		for(int i=1;i<k;i++){
+			target[0]=target[0]+(i*arr[i])-sum;
+			sum+=arr[i];
 		}
 
-		for(int i=2;i<=n;i++){
-			for(int j=1;j<=m;j++){
-				if(arr[i-1][j]<arr[i][j]){
-					dp[i][j]=dp[i-1][j];
-				}
-				else{
-					dp[i][j]=i;
-				}
-			}
+		for(int i=k;i<n;i++){
+			target.push_back(target[i-k]+((k-1LL)*arr[i])+((k-1LL)*arr[i-k])-(2LL*(sum-arr[i-k])));
+            sum-=arr[i-k];
+			sum+=arr[i];
 		}
+        
+        ll ans=target[0];
 
-		int ans[n+1];
-
-		for(int i=1;i<=n;i++){
-			ans[i]=dp[i][1];
-			for(int j=2;j<=m;j++){
-				ans[i]=min(ans[i],dp[i][j]);
-			}
+		for(int i=0;i<target.size();i++){
+			ans=min(ans,target[i]);
+            //cout<<target[i]<<" ";
 		}
-
-		int k;
-		cin>>k;
-		while(k--){
-			int l,r;
-			cin>>l>>r;
-			if(ans[r]<=l){
-				cout<<"Yes"<<endl;
-			}
-			else{
-				cout<<"No"<<endl;
-			}
-		}
-
+        cout<<ans<<endl;
 
 
 	}
