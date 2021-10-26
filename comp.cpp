c0c9476038e4b8c081c1e37defa04e4a13962014
Sakil Mostak <skmahim71@gmail.com>
2021-10-26 13:58:53 +0530

    Edit Distance(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -20,19 +20,19 @@ int main(){
 		}
 
 		for(int i=0;i<=m;i++){
-			dp[i][0]=0;
+			dp[i][0]=i;
 		}
 		for(int i=0;i<=n;i++){
-			dp[0][i]=0;
+			dp[0][i]=i;
 		}
 
 		for(int i=1;i<=m;i++){
 			for(int j=1;j<=n;j++){
-				if(s1[m-i]==s2[n-j]){
-					dp[i][j]=1+dp[i-1][j-1];
+				if(s1[n-i]==s2[n-i]){
+					dp[i][j]=dp[i-1][j-1];
 				}
 				else{
-					dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+					dp[i][j]=min(1+dp[i-1][j-1],min(1+dp[i-1][j],1+dp[i][j-1]));
 				}
 			}
 		}
