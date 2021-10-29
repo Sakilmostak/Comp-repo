c2d21941c3889da03ea04f7e4bae4ee7b414b215
Sakil Mostak <skmahim71@gmail.com>
2021-10-29 20:05:42 +0530

    Miser Man(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,71 +3,43 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
-struct items{
-	int profit;
-	int wt;
-};
-
-bool comp(items a,items b){
-	return a.profit<b.profit;
-}
-
 
 int main(){
 	
 		int t;
 		cin>>t;
 		while(t--){
-			int n,w;
-			cin>>n>>w;
-			items item[n];
-			for(int i=0;i<n;i++){
-				cin>>item[i].profit;
-			}
-
-			for(int i=0;i<n;i++){
-				cin>>item[i].wt;
-			}
-
-			sort(item,item+n,comp);
+			int n,m;
+			cin>>n>>m;
+			int fare[n][m+1];
 			
-			ll*** dp= new ll**[2];
-			for(int i=0;i<2;i++){
-				dp[i]=new ll*[n+1];
-				for(int j=0;j<=n;j++){
-					dp[i][j]= new ll[w+1];
-					for(int k=0;k<=w;k++){
-						dp[i][j][k]=0;
-					}
+			for(int i=0;i<n;i++){
+				for(int j=1;j<=m;j++){
+					cin>>fare[i][j];
 				}
 			}
 
-			int primes[11]= {1,2,3,5,7,11,13,17,19,23,29};
+			ll dp[n][m+2];
+			for(int i=0;i<n;i++){
+				dp[i][0]=INT_MAX;
+				dp[i][m+1]=INT_MAX;
+			}
+			for(int i=1;i<=m;i++){
+				dp[n-1][i]=fare[n-1][i];
+			}
 
-			for(int i=1;i<=n;i++){
-				for(int j=1;j<=w;j++){
-					dp[0][i][j]=dp[0][i-1][j];
-
-					if(item[i-1].wt<=j){
-						dp[0][i][j]=max(dp[0][i][j],dp[0][i-1][j-item[i-1].wt]+item[i-1].profit);
-					}
+			for(int i=n-2;i>=0;i--){
+				for(int j=1;j<=m;j++){
+					dp[i][j]=fare[i][j]+min(dp[i+1][j],min(dp[i+1][j-1],dp[i+1][j+1]));
 				}
 			}
 
-			for(int prime= 1;prime<=10;prime++){
-				int p=prime%2;
-				for(int i=1;i<=n;i++){
-					for(int j=1;j<=w;j++){
-						dp[p][i][j]=dp[p][i-1][j];
-						if(item[i-1].wt<=j){
-							dp[p][i][j]= max(dp[p][i][j],max(dp[p][i-1][j-item[i-1].wt]+item[i-1].profit,dp[p^1][i-1][j-item[i-1].wt]+ (item[i-1].profit*primes[prime])));
-						}
-					}
-				}
+			ll ans=INT_MAX;
+
+			for(int i=1;i<=m;i++){
+				ans=min(ans,dp[0][i]);
 			}
 
-			cout<<dp[0][n][w]<<endl;
-
-
+			cout<<ans<<endl;
 		}
 }
