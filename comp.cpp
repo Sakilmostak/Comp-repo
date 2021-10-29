c064c27599d4973e22a702a959e96c0796199bde
Sakil Mostak <skmahim71@gmail.com>
2021-10-29 21:15:20 +0530

    Trader Profit(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,43 +3,58 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
+ll findMaxStock(int arr[],int n,int k,int trans,ll*** dp){
+	if(n==0){
+		return 0L;
+	}
+	ll first=INT_MIN;
+	ll second=INT_MIN;
+	ll third=INT_MIN;
+
+	if(dp[trans][n][k]!=INT_MIN){
+		return dp[trans][n][k];
+	}
+
+	first= findMaxStock(arr+1,n-1,k,trans,dp);
+	if(trans==1){
+		second=findMaxStock(arr+1,n-1,k-1,0,dp)+arr[0];
+	}
+	if(trans==0 && k>0){
+		third=findMaxStock(arr+1,n-1,k,1,dp)-arr[0];
+	}
+
+	ll maxvalue= max(first,max(second,third));
+	dp[trans][n][k]=maxvalue;
+
+	return maxvalue;
+
+}
+
 
 int main(){
-	
-		int t;
-		cin>>t;
-		while(t--){
-			int n,m;
-			cin>>n>>m;
-			int fare[n][m+1];
-			
-			for(int i=0;i<n;i++){
-				for(int j=1;j<=m;j++){
-					cin>>fare[i][j];
-				}
-			}
-
-			ll dp[n][m+2];
-			for(int i=0;i<n;i++){
-				dp[i][0]=INT_MAX;
-				dp[i][m+1]=INT_MAX;
-			}
-			for(int i=1;i<=m;i++){
-				dp[n-1][i]=fare[n-1][i];
-			}
-
-			for(int i=n-2;i>=0;i--){
-				for(int j=1;j<=m;j++){
-					dp[i][j]=fare[i][j]+min(dp[i+1][j],min(dp[i+1][j-1],dp[i+1][j+1]));
-				}
-			}
-
-			ll ans=INT_MAX;
-
-			for(int i=1;i<=m;i++){
-				ans=min(ans,dp[0][i]);
-			}
-
-			cout<<ans<<endl;
+	int t;
+	cin>>t;
+	while(t--){
+		int k,n;
+		cin>>k;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++){
+			cin>>arr[i];
 		}
+
+		ll*** dp= new ll**[2];
+		for(int i=0;i<2;i++){
+			dp[i]= new ll*[n+1];
+			for(int j=0;j<=n;j++){
+				dp[i][j]= new ll[k+1];
+				for(int f=0;f<=k;f++){
+					dp[i][j][f]=INT_MIN;
+				}
+			}
+		}
+
+		ll ans= findMaxStock(arr,n,k,0,dp);
+		cout<<ans<<endl;
+	}
 }
