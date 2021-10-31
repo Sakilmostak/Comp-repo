4ec4e82c4d834d8ffa2ee1262ac8f1e26b31addc
Sakil Mostak <skmahim71@gmail.com>
2021-10-31 20:28:00 +0530

    Smaller Super-Sequence(Dynamic Programming)

--- comp.cpp
+++ comp.cpp
@@ -3,30 +3,61 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
+int shortSub(char a[], char b[],int n, int m, int** dp){
+	if(n==0 && m==0){
+		return 0;
+	}
+
+	if(dp[n][m]!=-1){
+		return dp[n][m];
+	}
+
+	if(n==0){
+		int third= 1+shortSub(a,b+1,0,m-1,dp);
+		dp[n][m]=third;
+		return third;
+	}
+	if(m==0){
+		int fourth= 1+shortSub(a+1,b,n-1,m,dp);
+		dp[n][m]=fourth;
+		return fourth;
+	}
+
+	if(a[0]==b[0]){
+		int fifth= 1+shortSub(a+1,b+1,n-1,m-1,dp);
+		dp[n][m]=fifth;
+		return fifth;
+	}
+	int first= 1+shortSub(a+1,b,n-1,m,dp);
+	int second= 1+shortSub(a,b+1,n,m-1,dp);
+
+	dp[n][m]= min(first,second);
+	return min(first,second);
+}
 
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		string s;
-		cin>>s;
-		int n=s.length();
-		int occured[256];
-		for(int i=0;i<256;i++){
-			occured[i]=-1%mod;
-		}
-		ll dp[n+1];
-		dp[0]=1;
-		for(int i=1;i<=n;i++){
-			dp[i]=(2L*dp[i-1])%mod;
-
-			if(occured[s[i-1]]!=-1){
-				dp[i]=(dp[i]%mod-dp[occured[s[i-1]]]+mod)%mod;
+		char a[501];
+		char b[501];
+		cin>>a;
+		cin>>b;
+		int n=strlen(a);
+		int m=strlen(b);
+		int** dp= new int*[n+1];
+		for(int i=0;i<=n;i++){
+			dp[i]=new int[m+1];
+			for(int j=0;j<=m;j++){
+				dp[i][j]=-1;
 			}
-
-			occured[s[i-1]]=i-1;
 		}
 
-		cout<<dp[n]%mod<<endl;
+		int ans= shortSub(a,b,n,m,dp);
+		for(int i=0;i<=n;i++){
+			delete [] dp[i];
+		}
+		delete [] dp;
+		cout<<ans<<endl;
 	}
 }
