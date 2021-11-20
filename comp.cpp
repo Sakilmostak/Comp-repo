0394527d4eaa1f78e9e9bf5c90a40338be031321
Sakil Mostak <skmahim71@gmail.com>
2021-11-20 12:37:51 +0530

    Dilemma(DP with Bitmasking)

--- comp.cpp
+++ comp.cpp
@@ -3,54 +3,69 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
-ll adjBitReq(int n,int k,int first,ll*** dp){
-	if(n==1){
-		if(k==0){
-			return 1;
-		}
-		if(k>0){
-			return 0;
-		}
-	}
-
-	if(k<0){
+ll finddiff(string s[], int n, int pos, ll mask, ll** dp){
+	if((mask&(mask-1))==0){
 		return 0;
 	}
 
-	if(first==1){
-		return adjBitReq(n-1,k-1,1,dp)+adjBitReq(n-1,k,0,dp);
+	if(pos<0){
+		return 10000;
 	}
-	else if(first==0){
-		return adjBitReq(n-1,k,1,dp)+adjBitReq(n-1,k,0,dp);
+    
+    if(dp[pos][mask]!=INT_MAX){
+        return dp[pos][mask];
+    }
+
+	ll mask1=0;
+	ll mask2=0;
+    int touches=0;
+	for(int i=0;i<n;i++){
+		if((mask&(1<<i))!=0){
+            touches++;
+			if(s[i][pos]=='0'){
+				mask1= mask1 | (1<<i);
+			}
+			if(s[i][pos]=='1'){
+				mask2= mask2 | (1<<i);
+			}
+		}	
 	}
+
+	ll first = finddiff(s,n,pos-1,mask1,dp);
+	ll second = finddiff(s,n,pos-1,mask2,dp);
+	ll third = finddiff(s,n,pos-1,mask,dp);
+
+	ll ans = min(first+second+touches,third);
+    dp[pos][mask]=ans;
+
+	return ans;
+
+
 }
-
-
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n,k;
-		cin>>n>>k;
-
-		ll*** dp=new ll**[2];
-		for(int i=0;i<2;i++){
-			dp[i]=new ll*[n+1];
-			for(int j=0;j<=n;j++){
-				dp[i][j]= new ll[k+1];
-				for(int f=0;f<=k;f++){
-					dp[i][j][f]=-1;
-				}
-			}
+		int n;
+		cin>>n;
+		string s[n];
+		for(int i=0;i<n;i++){
+			cin>>s[i];
 		}
+        
 
-		ll ans= adjBitReq(n+1,k,0,dp);
-		for(int i=0;i<2;i++){
-			for(int j=0;j<=n;j++){
-				delete [] dp[i][j];
-			}
-			delete [] dp[i];
-		}
+		int strsize= s[0].size();
+		ll mask = (1<<n)-1;
+        
+        ll ** dp= new ll*[strsize];
+        for(int i=0;i<strsize;i++){
+            dp[i]= new ll[(1<<n)];
+            for(int j=0;j<=mask;j++){
+                dp[i][j]=INT_MAX;
+            }
+        }
+
+		ll ans= finddiff(s,n,strsize-1,mask,dp);
 		cout<<ans<<endl;
 	}
 }
--- practice.cpp
+++ practice.cpp
@@ -3,58 +3,37 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
-int findways(int** like, int n){
-	
-	int* dp = new int[1<<n];
-	for(int i=0;i<(1<<n);i++){
+int totalspecialperm(int n){
+	int dp[1<<n];
+	int mask=(1<<n)-1;
+	for(int i=0;i<=mask;i++){
 		dp[i]=0;
 	}
 	dp[0]=1;
-
-	for(int mask=0;mask<((1<<n)-1);mask++){
-
-		int temp = mask;
-		int k=0;
-		while(temp>0){
-			if(temp%2==1){
-				k++;
-			}
-			temp=temp/2;
-		}
-
+	for(int i=0;i<mask;i++){
 		for(int j=0;j<n;j++){
-			if(!(mask&(1<<j)) && like[k][j]==1){
-				dp[mask | (1<<j)]+= dp[mask];
+			if((1<<j & i)==0){
+				bool flag=false;
+				for(int k=0;k<n;k++){
+					if(((j+1)&(k+1))==j+1 && ((1<<k)&i)){
+						flag=true;
+						break;
+					}
+				}
+				if(flag==false){
+					dp[1<<j | i]+=dp[i];
+				}
 			}
 		}
 	}
 
-	return dp[(1<<n)-1];
+	return dp[mask];
 }
 
+
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		int **like= new int*[n];
-		for(int i=0;i<n;i++){
-			like[i]= new int[n];
-		}
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				cin>>like[i][j];
-			}
-		}
-
-		int * dp = new int[1<<n];
-		dp[0]=1;
-		for(int i=0;i<(1<<n);i++){
-			dp[i]=0;
-		}
-
-		int ans= findways(like, n);
-		cout<<ans<<endl;
-	}
+	int n;
+	cin>>n;
+	int ans= totalspecialperm(n);
+	cout<<ans<<endl;
 }
