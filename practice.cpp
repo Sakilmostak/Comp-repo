31747be0ab20255daf38945c2f01ab23558befd2
Sakil Mostak <skmahim71@gmail.com>
2021-12-01 21:32:55 +0530

    Euclid Algorithm for GCD

--- practice.cpp
+++ practice.cpp
@@ -3,33 +3,30 @@ using namespace std;
 #define mod 1000000007
 #define ll long long
 
+ll euclidGcd(int a, int b){
+	if(b==0){
+		return a;
+	}
+
+	return euclidGcd(b, a%b);
+}
 
 int main(){
-	bool arr[10000001]={0};
-	for(int i=2;i<=1000;i++){
-		for(int j=i;j*i<=1000000;j++){
-            if(arr[i]==1){
-                break;
-            }
-            else{
-                arr[j*i]=1;
-            }
+	int t;
+	cin>>t;
+	while(t--){
+		ll x,y;
+		cin>>x>>y;
+
+		ll ans;
+
+		if(x>y){
+			ans=euclidGcd(x,y);
 		}
-	}
-
-	int n;
-	cin>>n;
-    if(n==1){
-        cout<<"0"<<endl;
-        return 0;
-    }
-	int count=1;
-	for(int i=3;i<=n;i++){
-		if(arr[i]==0){
-			count++;
+		else{
+			ans=euclidGcd(y,x);
 		}
+
+		cout<<ans<<endl;
 	}
-
-	cout<<count<<endl;
-
 }
