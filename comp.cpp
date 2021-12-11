589a7546484ae7ab2ba1509d009e20fda495f95d
Sakil Mostak <skmahim71@gmail.com>
2021-12-11 21:12:43 +0530

    Sum of LCM(Number Theory -using Totient function)

--- comp.cpp
+++ comp.cpp
@@ -2,30 +2,44 @@
 using namespace std;
 #define mod 1000000007
 #define ll long long
+ll phi[100002];
+ll ans[100002];
 
 int main(){
+	for (int i = 1; i <= 100000; i++) {
+        phi[i] = i;
+    }
+ 
+    for (int i = 2; i <= 100000; i++) {
+        if (phi[i] == i) {
+            phi[i] = i - 1;
+            for (int j = 2 * i; j <= 100000; j += i) {
+                phi[j] = (phi[j] * (i - 1)) / i;
+            }
+        }
+    }
+    
+    for (int i = 1; i <= 100000; i++) {
+ 
+        // Summation of d * ETF(d) where
+        // d belongs to set of divisors of n
+        for (int j = i; j <= 100000; j += i) {
+            ans[j] += (i * phi[i]);
+        }
+    }
+
 	int t;
 	cin>>t;
 	while(t--){
 		ll n; 
         cin>>n;
-		ll tot = n;
-		for (ll p = 2; p*p <= n; p++)
-		{
-    		if (n%p==0)
-    		{
-        		tot /= p;
-        		tot *= (p-1);
-        		while ( n % p == 0 ) 
-            		n /= p;
-    		}
-		}
-        
-		if ( n > 1 ) {
-    		tot /= n;
-    		tot *= (n-1);
-		}
-        
-        cout<<tot<<endl;
+		ll phisum=ans[n];
+
+		phisum+=1; // to add the extra one to it.
+
+		ll ans= (n*phisum)/2;
+
+		cout<<ans<<endl;
+		
 	}
 }
