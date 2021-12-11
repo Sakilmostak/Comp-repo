500f5cdfb5f3050f23ced5ca633b1e423e09a860
Sakil Mostak <skmahim71@gmail.com>
2021-12-11 22:08:59 +0530

    Segmented Sieve (Number Theory)

--- comp.cpp
+++ comp.cpp
@@ -2,44 +2,63 @@
 using namespace std;
 #define mod 1000000007
 #define ll long long
-ll phi[100002];
-ll ans[100002];
+#define MAX 100001
+
+vector<int>* Sieve(){
+	bool primes[MAX];
+	for(int i=2;i*i<MAX;i++){
+		if(!primes[i]){
+			for(int j=i*i;j<MAX;j+=i){
+				primes[j]=1;
+			}
+		}
+	}
+	vector<int>* primenum = new vector<int>();
+	primenum->push_back(2);
+	for(int i=3;i<MAX;i+=2){
+		if(!primes[i]){
+			primenum->push_back(i);
+		}
+	}
+
+	return primenum;
+}
+
+void printPrimes(ll l, ll r, vector<int>* & primes){
+	bool isPrime[r-l+1]={0};
+
+	for(int i=0;primes->at(i)*(ll)primes->at(i)<=r;i++){
+		int currPrime = primes->at(i);
+		ll base= (l/(currPrime))*currPrime;  //just smaller or equal value to l whose mod is 0
+		if(base<l){
+			base+=currPrime;
+		}
+		for(ll j=base;j<=r;j+=currPrime){
+			isPrime[j-l]=1;
+		}
+		if(base == currPrime){  //if base itself is a prime number
+			isPrime[base-l]=0;
+		}
+	}
+    if(l==1){
+        isPrime[0]=1;
+    }
+
+	for(ll i=0;i<=r-l;i++){
+		if(!isPrime[i]){
+			cout<<i+l<<" ";
+		}
+	}
+    cout<<endl;
+}
 
 int main(){
-	for (int i = 1; i <= 100000; i++) {
-        phi[i] = i;
-    }
- 
-    for (int i = 2; i <= 100000; i++) {
-        if (phi[i] == i) {
-            phi[i] = i - 1;
-            for (int j = 2 * i; j <= 100000; j += i) {
-                phi[j] = (phi[j] * (i - 1)) / i;
-            }
-        }
-    }
-    
-    for (int i = 1; i <= 100000; i++) {
- 
-        // Summation of d * ETF(d) where
-        // d belongs to set of divisors of n
-        for (int j = i; j <= 100000; j += i) {
-            ans[j] += (i * phi[i]);
-        }
-    }
-
+	vector<int>* primes = Sieve();
 	int t;
 	cin>>t;
 	while(t--){
-		ll n; 
-        cin>>n;
-		ll phisum=ans[n];
-
-		phisum+=1; // to add the extra one to it.
-
-		ll ans= (n*phisum)/2;
-
-		cout<<ans<<endl;
-		
+		ll l,r;
+		cin>>l>>r;
+		printPrimes(l,r,primes);
 	}
 }
