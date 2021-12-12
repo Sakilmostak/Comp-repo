713eccb99a3f8daf39455e1e2c0835a5c7b3ec0d
Sakil Mostak <skmahim71@gmail.com>
2021-12-12 11:34:43 +0530

    Modular Exponentiation (Number Theory)

--- comp.cpp
+++ comp.cpp
@@ -4,61 +4,33 @@ using namespace std;
 #define ll long long
 #define MAX 100001
 
-vector<int>* Sieve(){
-	bool primes[MAX];
-	for(int i=2;i*i<MAX;i++){
-		if(!primes[i]){
-			for(int j=i*i;j<MAX;j+=i){
-				primes[j]=1;
-			}
-		}
+int modExp(int a, int b, int c){
+	if(a==0){
+		return 0;
 	}
-	vector<int>* primenum = new vector<int>();
-	primenum->push_back(2);
-	for(int i=3;i<MAX;i+=2){
-		if(!primes[i]){
-			primenum->push_back(i);
-		}
+	if(b==0){
+		return 1;
 	}
 
-	return primenum;
-}
-
-void printPrimes(ll l, ll r, vector<int>* & primes){
-	bool isPrime[r-l+1]={0};
-
-	for(int i=0;primes->at(i)*(ll)primes->at(i)<=r;i++){
-		int currPrime = primes->at(i);
-		ll base= (l/(currPrime))*currPrime;  //just smaller or equal value to l whose mod is 0
-		if(base<l){
-			base+=currPrime;
-		}
-		for(ll j=base;j<=r;j+=currPrime){
-			isPrime[j-l]=1;
-		}
-		if(base == currPrime){  //if base itself is a prime number
-			isPrime[base-l]=0;
-		}
+	long ans;
+	if(b%2==0){
+		long smallAns= modExp(a,b/2,c);
+		ans= (smallAns*smallAns)%c;
 	}
-    if(l==1){
-        isPrime[0]=1;
-    }
-
-	for(ll i=0;i<=r-l;i++){
-		if(!isPrime[i]){
-			cout<<i+l<<" ";
-		}
+	else{
+		long smallAns= modExp(a,b-1,c);
+		ans= (a%c);
+		ans= (ans*smallAns)%c;
 	}
-    cout<<endl;
+
+	return int((ans+c)%c);
 }
 
 int main(){
-	vector<int>* primes = Sieve();
-	int t;
-	cin>>t;
-	while(t--){
-		ll l,r;
-		cin>>l>>r;
-		printPrimes(l,r,primes);
-	}
+	int a,b,c;
+	cin>>a>>b>>c;
+
+	cout<<modExp(a,b,c)<<endl;
+
+	return 0;
 }
