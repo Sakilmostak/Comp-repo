2e53fa3bd781b789cdf1bce25ef33da29f0b9b59
Sakil Mostak <skmahim71@gmail.com>
2021-12-13 13:05:19 +0530

    GCD Extreme(Number Theory)

--- comp.cpp
+++ comp.cpp
@@ -1,47 +1,66 @@
-#include<bits/stdc++.h>
+#include <iostream>
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define endl '\n'
+#define int long long int
+#define MAX 1000001
+
 using namespace std;
 
-int power(long long int a, string b, int m)
+int phi[MAX];
+unsigned int S[MAX], G[MAX];
+
+int32_t main()
 {
-	int len = b.length();
-	long long int final_ans = 1;
-	for(int i=len-1;i>=0;i--)
-	{
-		if(b[i] == '0')
-		{	
-			a %= m;
-			a = (((a*a)%m)*a)%m;
-		}
-		if(b[i] == '1')
-		{	
-			final_ans = final_ans * a;
-			final_ans %= m;
-			a = (((a*a)%m)*a)%m;
-		}
-		if(b[i] == '2')
-		{
-			final_ans = ((final_ans * a)%m) * a;
-			final_ans %= m;
-			a = (((a*a)%m)*a)%m;
-		}
-	}
-	return final_ans;
-}
+    fast unsigned int i, j, n;
+    phi[1] = 1;
 
-int main()
-{
-	long long int t;
-	cin >> t;
-	while(t--)
-	{
-		long long int a; //in base 10
-		string b; //in base 3
-		int m; //in base 10
-		cin >> a >> b >> m;
+    for (i = 2; i < MAX; i += 2)
+    {
+        phi[i] = i / 2;
+    }
 
-		cout << power(a,b,m) << endl;
-	}
+    for (i = 3; i < MAX; i += 2)
+    {
+        if (!phi[i])
+        {
+            phi[i] = i - 1;
+            for (j = i << 1; j < MAX; j += i)
+            {
+                if (!phi[j])
+                {
+                    phi[j] = j;
+                }
 
-	return 0 ; 
+                phi[j] = phi[j] / i * (i - 1);
+            }
+        }
+    }
 
+    for (i = 0; i < MAX; i++)
+        S[i] = phi[i];
+
+    for (i = 2; i < MAX; i++)
+    {
+        for (j = 2; j * i < MAX; j++)
+        {
+            S[i * j] += j * phi[i];
+        }
+    }
+    
+    G[1] = 0;
+    for (i = 2; i < MAX; i++)
+        G[i] = G[i - 1] + S[i];
+    
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        cin >> n;
+        if (n == 0)
+            break;
+        cout << G[n] << endl;
+    }
 }
