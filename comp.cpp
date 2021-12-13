afc0da99c57445c60592cd7b005f99f7b1d2bf16
Sakil Mostak <skmahim71@gmail.com>
2021-12-13 13:03:40 +0530

    Cubic Square(Number Theory)

--- comp.cpp
+++ comp.cpp
@@ -1,94 +1,47 @@
-#include<iostream>
-#define ll long long
-#define mod 1000000007
+#include<bits/stdc++.h>
 using namespace std;
-ll fib(ll n)
+
+int power(long long int a, string b, int m)
 {
-    if (n == 0 || n == 1 || n == 5)
-        return n;
-    if (n == 2)
-        return 1;
-    n--;
-    ll a[2][2] = {1, 1,
-                  1, 0};
-    ll ans[2][2] = {
-        1, 0,
-        0, 1};
-    ll temp[2][2];
-    ll m = mod - 1, i, j, k;
-    while (n)
-    {
-        if (n & 1)
-        {
-            //ans=ans*a
-            for (i = 0; i < 2; i++)
-                for (j = 0; j < 2; j++)
-                {
-                    temp[i][j] = 0;
-                    for (k = 0; k < 2; k++)
-                    {
-                        temp[i][j] += a[i][k] * ans[k][j];
-                        temp[i][j] %= m;
-                    }
-                }
-            for (i = 0; i < 2; i++)
-                for (j = 0; j < 2; j++)
-                    ans[i][j] = temp[i][j];
-        }
-        //a=a*a
-        for (i = 0; i < 2; i++)
-            for (j = 0; j < 2; j++)
-            {
-                temp[i][j] = 0;
-                for (k = 0; k < 2; k++)
-                {
-                    temp[i][j] += a[i][k] * a[k][j];
-                    temp[i][j] %= m;
-                }
-            }
-        for (i = 0; i < 2; i++)
-            for (j = 0; j < 2; j++)
-                a[i][j] = temp[i][j];
-        n >>= 1;
-    }
-    return ans[0][0];
+	int len = b.length();
+	long long int final_ans = 1;
+	for(int i=len-1;i>=0;i--)
+	{
+		if(b[i] == '0')
+		{	
+			a %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+		if(b[i] == '1')
+		{	
+			final_ans = final_ans * a;
+			final_ans %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+		if(b[i] == '2')
+		{
+			final_ans = ((final_ans * a)%m) * a;
+			final_ans %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+	}
+	return final_ans;
 }
-ll mpow(ll a, ll b)
-{
-    ll ans = 1;
-    while (b)
-    {
-        if (b & 1)
-            ans = (ans * a) % mod;
-        a = (a * a) % mod;
-        b >>= 1;
-    }
-    return ans;
-}
+
 int main()
 {
-    int t;
-    ll a, b, c, n, i, x, y;
-    cin >> t;
-    while (t--)
-    {
-        cin >> a >> b >> n;
-        if (n == 0)
-            cout << a << endl;
-        else if (n == 1)
-            cout << b << endl;
-        else
-        {
-            x = fib(n - 1);
-            y = fib(n);
-            //cout<<x<<" "<<y<<endl;
-            c = mpow(a + 1, x) * mpow(b + 1, y);
-            c--;
-            c = c % mod;
-            if (c < 0)
-                c += mod;
-            cout << c << endl;
-        }
-    }
-    return 0;
+	long long int t;
+	cin >> t;
+	while(t--)
+	{
+		long long int a; //in base 10
+		string b; //in base 3
+		int m; //in base 10
+		cin >> a >> b >> m;
+
+		cout << power(a,b,m) << endl;
+	}
+
+	return 0 ; 
+
 }
