de17fb5234ccd03200f378ecfe5fc44b2174091f
Sakil Mostak <skmahim71@gmail.com>
2021-12-13 21:11:41 +0530

    Sehway and ETF (Number Theory)

--- comp.cpp
+++ comp.cpp
@@ -1,51 +1,85 @@
-#include <iostream>
-#define endl '\n'
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0);
+#include<bits/stdc++.h>
 #define ll long long
-#define MAX 1000001
-#define mod 1000000007
+#define MAX 1100001
+#define ss(n) scanf("%lld", &n)
 using namespace std;
-ll fact[MAX];
-ll modexpo(ll a, ll b)
-{
-    ll ans = 1;
-    while (b)
-    {
-        if (b & 1)
-            ans = (ans * a) % mod;
-        a = (a * a) % mod;
-        b >>= 1;
-    }
-    return ans;
-}
+bool primes[MAX];
+ll phi[MAX];
+ll p[300001];
+ll num[MAX];
 int main()
 {
-    fast
-        ll n,
-        i, t, k, ans;
-    fact[0] = 1;
-    for (i = 1; i < MAX; i++)
-    {
-        fact[i] = i * fact[i - 1];
-        if (fact[i] >= mod)
-            fact[i] %= mod;
-    }
+    ll t, a, b, k, i, j, x, base;
     cin >> t;
+    for (i = 3; i < MAX; i += 2)
+        primes[i] = 1;
+    primes[2] = 1;
+    for (i = 3; i * i < MAX; i += 2)
+    {
+        if (primes[i])
+        {
+            for (j = i * i; j < MAX; j += 2 * i)
+                primes[j] = 0;
+        }
+    }
+    p[0] = 2;
+    ll c = 1;
+    for (i = 3; i < MAX; i += 2)
+    {
+        if (primes[i])
+            p[c++] = i;
+    }
     while (t--)
     {
-        cin >> n >> k;
-        if (k > n)
+        cin >> a >> b >> k;
+        if (k == 1)
         {
-            cout << endl;
+            std::cout << std::fixed;
+            std::cout << std::setprecision(6) << 1 << endl;
             continue;
         }
-        ans = modexpo(2, k);
-        ans = (ans * fact[n]) % mod;
-        ans = (ans * modexpo(fact[k], mod - 2)) % mod;
-        ans = (ans * modexpo(fact[n - k], mod - 2)) % mod;
-        cout << ans << endl;
+        for (i = a; i <= b; i++)
+        {
+            phi[i - a] = i;
+            num[i - a] = i;
+        }
+        for (i = 0; p[i] * p[i] <= b; i++)
+        {
+            base = a / p[i] * p[i];
+            while (base < a)
+                base += p[i];
+            while (base < p[i])
+                base += p[i];
+            if (base == p[i])
+            {
+                // cout<<phi[base-a]<<endl;
+                base += p[i];
+            }
+            for (j = base; j <= b; j += p[i])
+            {
+                while (num[j - a] % p[i] == 0)
+                    num[j - a] /= p[i];
+                phi[j - a] -= phi[j - a] / p[i];
+                //cout<<j<<" "<<phi[j-a]<<endl;
+            }
+        }
+        for (i = a; i <= b; i++)
+        {
+            if (num[i - a] > 1)
+                phi[i - a] -= phi[i - a] / num[i - a];
+            num[i - a] = 1;
+        }
+        ll c = 0;
+        for (i = a; i <= b; i++)
+        {
+            //cout<<i<<" "<<phi[i-a]<<endl;
+            if (phi[i - a] % k == 0)
+                c++;
+        }
+        double ans = c;
+        ans /= (b - a + 1);
+        std::cout << std::fixed;
+        std::cout << std::setprecision(6) << ans << endl;
     }
     return 0;
 }
