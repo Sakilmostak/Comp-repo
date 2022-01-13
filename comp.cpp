bb0bc3c20fb4b0d731eee92ab5586f7c85490be8
Sakil Mostak <skmahim71@gmail.com>
2022-01-13 11:48:56 +0530

    Maximum Query(Range Query 1)

--- comp.cpp
+++ comp.cpp
@@ -5,59 +5,55 @@
 #define endl "\n"
 using namespace std;
 
-struct enemy{
-    public:
-    ll a;
-    ll b;
-};
+void buildtree(ll arr[], ll *tree, int start, int end, int treeidx){
+    if(start==end){
+        tree[treeidx]= arr[start];
+        return;
+    }
+
+    int mid = (start+end)/2;
+    buildtree(arr,tree,start,mid,2*treeidx);
+    buildtree(arr,tree,mid+1,end,2*treeidx+1);
+
+    tree[treeidx]= max(tree[2*treeidx],tree[2*treeidx+1]);
+
+}
+
+ll query(ll arr[], ll* tree, int start, int end, int treeidx, int left, int right){
+    if(right< start || left> end){
+        return -1;
+    }
+
+    if(left<=start && right>=end){
+        return tree[treeidx];
+    }
+
+    int mid = (start+end)/2;
+    ll ans1 = query(arr,tree,start,mid,2*treeidx,left,right);
+    ll ans2 = query(arr,tree,mid+1,end,2*treeidx+1,left,right);
+    return max(ans1,ans2);
+}
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        struct enemy arr[n];
-        for(int i=0;i<n;i++){
-            cin>>arr[i].a;
-            cin>>arr[i].b;
-        }
+    int n;
+    cin>>n;
+    ll arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    ll *tree= new ll[4*n];
+    buildtree(arr,tree,0,n-1,1);
 
-        ll dp[n];
-        dp[0]=arr[0].a-arr[n-1].b;
-
-        for(int i=1;i<n;i++){
-            dp[i]=arr[i].a-arr[i-1].b;
-        }
-
-        ll total=0;
-
-        for(int i=0;i<n;i++){
-            if(dp[i]>0){
-                total+=dp[i];
-            }
-        }
-
-        ll ans[n];
-
-        for(int i=0;i<n;i++){
-            ans[i]=total;
-            if(dp[i]>0){
-                ans[i]-=dp[i];
-            }
-
-            ans[i]+=arr[i].a;
-            
-        }
-
-        ll crrans=INT64_MAX;
-
-        for(int i=0;i<n;i++){
-            crrans= min(crrans, ans[i]);
-        }
-
-        cout<<crrans<<endl;
-
+    ll q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        ll ans= query(arr,tree,0,n-1,1,l,r);
+        cout<<ans<<endl;
 
     }
+
+    delete [] tree;
+
 }
