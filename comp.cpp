18bd128d3f98a5326274672e53fd0fa3e2bab620
Sakil Mostak <skmahim71@gmail.com>
2022-01-13 12:44:01 +0530

    Counting Even/Odd(Range Query 1)

--- comp.cpp
+++ comp.cpp
@@ -5,33 +5,84 @@
 #define endl "\n"
 using namespace std;
 
-void buildtree(ll arr[], ll *tree, int start, int end, int treeidx){
+void buildtree(ll arr[], ll *oddtree, ll* eventree, int start, int end, int treeidx){
     if(start==end){
-        tree[treeidx]= arr[start];
-        return;
+        if(arr[start]&1 == 1){
+            oddtree[treeidx]=1;
+            eventree[treeidx]=0;
+            return;
+        }
+        else{
+            oddtree[treeidx]=0;
+            eventree[treeidx]=1;
+            return;
+        }
     }
 
     int mid = (start+end)/2;
-    buildtree(arr,tree,start,mid,2*treeidx);
-    buildtree(arr,tree,mid+1,end,2*treeidx+1);
+    buildtree(arr,oddtree,eventree,start,mid,2*treeidx);
+    buildtree(arr,oddtree,eventree,mid+1,end,2*treeidx+1);
 
-    tree[treeidx]= max(tree[2*treeidx],tree[2*treeidx+1]);
+    oddtree[treeidx]=oddtree[2*treeidx]+oddtree[2*treeidx+1];
+    eventree[treeidx]=eventree[2*treeidx]+eventree[2*treeidx+1];
+
 
 }
 
-ll query(ll arr[], ll* tree, int start, int end, int treeidx, int left, int right){
+void updateTree(ll arr[], ll* oddtree, ll* eventree, int start, int end, int treeidx, int idx, int value){
+    if(start==end){
+        arr[start]=value;
+        if(value&1==1){
+            oddtree[treeidx]=1;
+            eventree[treeidx]=0;
+        }
+        else{
+            oddtree[treeidx]=0;
+            eventree[treeidx]=1;
+        }
+
+        return;
+    }
+
+    int mid= (start+end)/2;
+    if(mid<idx){
+        updateTree(arr,oddtree,eventree,mid+1,end,2*treeidx+1,idx,value);
+    }
+    else{
+        updateTree(arr,oddtree,eventree,start,mid,2*treeidx,idx,value);
+    }
+    oddtree[treeidx]=oddtree[2*treeidx]+oddtree[2*treeidx+1];
+    eventree[treeidx]= eventree[2*treeidx]+eventree[2*treeidx+1];
+}
+
+int queryEven(ll arr[], ll* eventree, int start, int end, int treeidx, int left, int right){
     if(right< start || left> end){
-        return -1;
+        return 0;
     }
 
     if(left<=start && right>=end){
-        return tree[treeidx];
+        return eventree[treeidx];
     }
 
     int mid = (start+end)/2;
-    ll ans1 = query(arr,tree,start,mid,2*treeidx,left,right);
-    ll ans2 = query(arr,tree,mid+1,end,2*treeidx+1,left,right);
-    return max(ans1,ans2);
+    int ans1 = queryEven(arr,eventree,start,mid,2*treeidx,left,right);
+    int ans2 = queryEven(arr,eventree,mid+1,end,2*treeidx+1,left,right);
+    return ans1+ans2;
+}
+
+int queryOdd(ll arr[], ll* oddtree, int start, int end, int treeidx, int left, int right){
+    if(right< start || left> end){
+        return 0;
+    }
+
+    if(left<=start && right>=end){
+        return oddtree[treeidx];
+    }
+
+    int mid = (start+end)/2;
+    int ans1 = queryOdd(arr,oddtree,start,mid,2*treeidx,left,right);
+    int ans2 = queryOdd(arr,oddtree,mid+1,end,2*treeidx+1,left,right);
+    return ans1+ans2;
 }
 
 int main(){
@@ -41,19 +92,29 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    ll *tree= new ll[4*n];
-    buildtree(arr,tree,0,n-1,1);
+    ll *oddtree= new ll[4*n];
+    ll *eventree= new ll[4*n];
+    buildtree(arr,oddtree,eventree,0,n-1,1);
 
-    ll q;
+    int q;
     cin>>q;
     while(q--){
-        int l,r;
-        cin>>l>>r;
-        ll ans= query(arr,tree,0,n-1,1,l,r);
-        cout<<ans<<endl;
-
+        ll check,x,y;
+        cin>>check>>x>>y;
+        if(check==0){
+            updateTree(arr,oddtree,eventree,0,n-1,1,x-1,y);
+        }
+        else if(check==1){
+            int ans=queryEven(arr,eventree,0,n-1,1,x-1,y-1);
+            cout<<ans<<endl;
+        }
+        else if(check==2){
+            int ans=queryOdd(arr,oddtree,0,n-1,1,x-1,y-1);
+            cout<<ans<<endl;
+        }
     }
 
-    delete [] tree;
+    
+
 
 }
