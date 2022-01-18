c0cd4c4f4f0ca1948e4b7a6ff31e140090f2274e
Sakil Mostak <skmahim71@gmail.com>
2022-01-18 20:31:33 +0530

    Has Path(Graph)

--- comp.cpp
+++ comp.cpp
@@ -5,60 +5,60 @@
 #define endl "\n"
 using namespace std;
 
-void BFS(bool** adjgraph,bool* visited, int n, int startvalue);
-
-void check(bool** adjgraph, bool* visited, int n){
+bool findPath(bool** adjgraph,bool* visited,int n, int v1,int v2){
+	if(adjgraph[v1][v2] || v1==v2){
+		return true;
+	}
+	visited[v1]=1;
 	for(int i=0;i<n;i++){
-		if(!visited[i]){
-			BFS(adjgraph,visited,n,i);
-		}
-	}
-}
-
-void BFS(bool** adjgraph ,bool* visited,int n, int startvalue){
-	queue<int> q;
-	if(!visited[startvalue]){
-		cout<<startvalue<<" ";
-		visited[startvalue]=1;
-		q.push(startvalue);
-	}
-
-	while(!q.empty()){
-		int key=q.front();
-		q.pop();
-		for(int i=0;i<n;i++){
-			if(adjgraph[key][i] && !visited[i]){
-				cout<<i<<" ";
-				q.push(i);
-				visited[i]=1;
+		if(adjgraph[i][v1] && !visited[i]){
+			if(i==v2){
+				return true;
+			}
+			bool check=findPath(adjgraph,visited,n,i,v2);
+			if(check){
+				return true;
 			}
 		}
 	}
 
-	check(adjgraph,visited,n);
+	return false;
 }
 
 
 int main(){
-  int n,e;
-  cin>>n>>e;
 
-  bool** adjgraph= new bool* [n];
-  for(int i=0;i<n;i++){
-	  adjgraph[i]= new bool[n];
-	  for(int j=0;j<n;j++){
-		  adjgraph[i][j]=0;
-	  }
-  }
+	int t;
+	cin>>t;
+	while(t--){
+		int n,e;
+		cin>>n>>e;
 
-  for(int i=0;i<e;i++){
-	  int a,b;
-	  cin>>a>>b;
-	  adjgraph[a][b]=1;
-	  adjgraph[b][a]=1;
-  }
+		bool** adjgraph= new bool* [n];
+		for(int i=0;i<n;i++){
+			adjgraph[i]= new bool[n];
+			for(int j=0;j<n;j++){
+				adjgraph[i][j]=0;
+			}
+		}
 
-  bool* visited= new bool[n]();
-  BFS(adjgraph,visited,n,0);
+		for(int i=0;i<e;i++){
+			int a,b;
+			cin>>a>>b;
+			adjgraph[a][b]=1;
+			adjgraph[b][a]=1;
+		}
 
+		int v1,v2;
+		cin>>v1>>v2;
+
+		bool* visited= new bool[n]();
+		bool ans=findPath(adjgraph,visited,n,v1,v2);
+		if(ans){
+			cout<<"true"<<endl;
+		}
+		else{
+			cout<<"false"<<endl;
+		}
+	}
 }
