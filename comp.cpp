b9e38a79020e4bb6909d40d1b6faa4acfa14b128
Sakil Mostak <skmahim71@gmail.com>
2022-01-21 12:18:13 +0530

    Coding Ninjas(Graph)- my code

--- comp.cpp
+++ comp.cpp
@@ -5,52 +5,129 @@
 #define endl "\n"
 using namespace std;
 
-void DFS(bool** adjgraph, bool* visited, int n, int startvalue){
-	if(!visited[startvalue]){
-		visited[startvalue]=1;
-	}
+bool visited[1000][1000];
 
-	for(int i=0;i<n;i++){
-		if(adjgraph[startvalue][i] && !visited[i]){
-			DFS(adjgraph,visited,n,i);
-		}
-	}
+bool validity(int x, int y, int n, int m){
+    if(x>=0 && x<n && y>=0 && y<m){
+        return true;
+    }
+
+    return false;
+}
+
+bool findingNinjas(char** charmatrix, int x, int y, int n,int m, string refer, int index){
+    if(!visited[x][y]){
+        visited[x][y]=1;
+    }
+    if(index==(refer.size()-1) && charmatrix[x][y]==refer[index]){
+        return true;
+    }
+
+
+    if(validity(x-1,y,n,m) && charmatrix[x-1][y]==refer[index+1] && index<refer.size() && !visited[x-1][y]){
+        bool check=findingNinjas(charmatrix,x-1,y,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x-1,y+1,n,m) && charmatrix[x-1][y+1]==refer[index+1] && index<refer.size() && !visited[x-1][y+1]){
+        bool check=findingNinjas(charmatrix,x-1,y+1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x,y+1,n,m) && charmatrix[x][y+1]==refer[index+1] && index<refer.size() && !visited[x][y+1]){
+        bool check=findingNinjas(charmatrix,x,y+1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x+1,y+1,n,m) && charmatrix[x+1][y+1]==refer[index+1] && index<refer.size() && !visited[x+1][y+1]){
+        bool check=findingNinjas(charmatrix,x+1,y+1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x+1,y,n,m) && charmatrix[x+1][y]==refer[index+1] && index<refer.size() && !visited[x+1][y]){
+        bool check=findingNinjas(charmatrix,x+1,y,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x+1,y-1,n,m) && charmatrix[x+1][y-1]==refer[index+1] && index<refer.size() && !visited[x+1][y-1]){
+        bool check=findingNinjas(charmatrix,x+1,y-1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x,y-1,n,m) && charmatrix[x][y-1]==refer[index+1] && index<refer.size() && !visited[x][y-1]){
+        bool check=findingNinjas(charmatrix,x,y-1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+    if(validity(x-1,y-1,n,m) && charmatrix[x-1][y-1]==refer[index+1] && index<refer.size() && !visited[x-1][y-1]){
+        bool check=findingNinjas(charmatrix,x-1,y-1,n,m,refer,index+1);
+        if(check){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int main(){
-	int t;
+    int t;
     cin>>t;
     while(t--){
-		int n,e;
-		cin>>n>>e;
+        int n,m;
+        cin>>n>>m;
+        char** charmatrix= new char*[n];
+        for(int i=0;i<n;i++){
+            charmatrix[i]= new char[m];
+        }
 
-		bool** adjgraph= new bool* [n];
-		for(int i=0;i<n;i++){
-			adjgraph[i]= new bool[n];
-			for(int j=0;j<n;j++){
-				adjgraph[i][j]=0;
-			}
-		}
-
-		for(int i=0;i<e;i++){
-			int a,b;
-			cin>>a>>b;
-			adjgraph[a][b]=1;
-			adjgraph[b][a]=1;
-		}
-
-		bool* visited= new bool[n]();
-        int ans=0;
-		for(int i=0;i<n;i++){
-            if(!visited[i]){
-                DFS(adjgraph,visited,n,i);
-                ans++;
+        for(int i=0;i<n;i++){
+            string s;
+            cin>>s;
+            for(int j=0;j<m;j++){
+                charmatrix[i][j]=s[j];
             }
         }
-        
-        cout<<ans<<endl;
-		
-	}
+
+        string refer= "CODINGNINJA";
+        bool ans=false;
+        bool goOut=false;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(charmatrix[i][j]==refer[0]){
+                    ans= findingNinjas(charmatrix,i,j,n,m,refer,0);
+                    if(ans){
+                        cout<<"1"<<endl;
+                        goOut=true;
+                        break;
+                    }
+                    memset(visited,0,sizeof(visited));
+                }
+            }
+
+            if(goOut){
+                break;
+            }
+        }
+
+        if(!ans){
+            cout<<"0"<<endl;
+        }
+
+        for(int i=0;i<n;i++){
+            delete [] charmatrix[i];
+        }
+
+        delete [] charmatrix;
 
 
+
+    }
 }
