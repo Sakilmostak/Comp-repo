e144c3f27ca83f4a67ad252e53bfdbfce42ed0d2
Sakil Mostak <skmahim71@gmail.com>
2022-02-01 21:42:59 +0530

    Floyd-Warshall Algorithm(Graph)

--- comp.cpp
+++ comp.cpp
@@ -5,56 +5,59 @@
 #define endl "\n"
 using namespace std;
 
-class directedEdge{
-	public:
-	int a,b,weight;
-};
-
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n,m,src,des;
+		int n,m;
 		cin>>n>>m;
-		cin>>src>>des;
-
-		directedEdge diredge[m];
+		int** adjgraph= new int*[n+1];
+		for(int i=0;i<=n;i++){
+			adjgraph[i]=new int[n+1];
+			for(int j=0;j<=n;j++){
+				adjgraph[i][j]=1e9;
+			}
+		}
 
 		for(int i=0;i<m;i++){
-			cin>>diredge[i].a;
-			cin>>diredge[i].b;
-			cin>>diredge[i].weight;
+			int src;
+			int des;
+			int weight;
+			cin>>src>>des>>weight;
+			adjgraph[src][des]=min(adjgraph[src][des],weight);
+			adjgraph[des][src]=min(adjgraph[des][src],weight);
 		}
 
-		int arr[n];
-
-		for(int i=0;i<n;i++){
-			arr[i]= 100001;
+		//bellman ford algorithm
+		int dist[n+1][n+1];
+		for(int i=0;i<=n;i++){
+			for(int j=0;j<=n;j++){
+				dist[i][j]=adjgraph[i][j];
+			}
 		}
 
-		arr[src]=0;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(arr[diredge[j].a]+diredge[j].weight<arr[diredge[j].b]){
-					arr[diredge[j].b]=arr[diredge[j].a]+diredge[j].weight;
+		for(int k=1;k<=n;k++){
+			for(int i=1;i<=n;i++){
+				for(int j=1;j<=n;j++){
+					if(dist[i][k]!=1e9 && dist[k][j]!=1e9 && dist[i][k]+dist[k][j]<dist[i][j]){
+						dist[i][j]=dist[i][k]+dist[k][j];
+					}
 				}
 			}
 		}
 
-		bool check=true;
-
-		for(int i=0;i<m;i++){
-			if(arr[diredge[i].a]+diredge[i].weight<arr[diredge[i].b]){
-				check=false;
+		//queries
+		int q;
+		cin>>q;
+		for(int i=0;i<q;i++){
+			int u,v;
+			cin>>u>>v;
+			if(u==v){
+				cout<<0<<endl;
+			}
+			else{
+				cout<<dist[u][v]<<endl;
 			}
 		}
-
-		if(check){
-			cout<<arr[des]<<endl;
-		}
-
-
-
-
 	}
 }
