972f2692948ba53ff92bee58a8cfe3172477aa35
Sakil Mostak <skmahim71@gmail.com>
2022-02-10 09:54:41 +0530

    Longest Palindromic Substring(Optimized)

--- comp.cpp
+++ comp.cpp
@@ -1,55 +1,32 @@
 #include<bits/stdc++.h>
-#define ull unsigned long long
-#define ll long long
-#define mod 1000000007
-#define endl "\n"
 using namespace std;
-
-int LPS(string s){
-	string newstr;
-	newstr+="$";
-	for(int i=0;i<s.size();i++){
-		newstr=newstr+"#"+s[i];
-	}
-	newstr+="#@";
-	
-	//Manacher's Algorithm
-	int n= newstr.size();
-	int *P= new int[n]();    // initialize to zero
-	int center=0,right=0;
-
-	for(int i=1;i<n-1;i++){
-		int mirror=(2*center) - i;
-		
-		if(i<right){
-			P[i]=min(right-i,P[mirror]);
-		}
-
-		while((i+1+P[i])<n && (i-1-P[i])>=0 && newstr[i+1+P[i]]==newstr[i-1-P[i]]){
-			P[i]++;
-		}
-
-		if(i+P[i]>right){
-			center=i;
-			right=i+P[i];
-		}
-	}
-
-	// finding longest palindrome
-	int ans=1;
-	for(int i=0;i<n;i++){
-		ans=max(ans,P[i]);
-	}
-
-	delete[] P;
-
-	return ans;
-
+int helper(string s,int n){
+    vector<int> v(n);
+    for(int i=0,l=0,r=-1;i<n;i++){
+        int k=(i>r)?1:min(v[l+r-i],r-i+1);
+        while(0<=i-k and i+k<n and s[i-k]==s[i+k])
+            k++;
+        v[i]=k--;
+        if(i+k>r){
+            l=i-k;r=i+k;
+        }
+    }vector<int> v1(n);
+        for(int i=0,l=0,r=-1;i<n;i++){
+        int k=(i>r)?0:min(v1[l+r-i+1],r-i+1);
+        while(0<=i-k-1 and i+k<n and s[i-k-1]==s[i+k])
+            k++;
+        v1[i]=k--;
+            if(i+k>r){
+                l=i-k-1;
+                r=i+k;
+            }
+    }int ans=1;
+    for(int i=0;i<n;i++){
+        ans=max(ans,max(2*v[i]-1,2*v1[i]));
+    }return ans;
 }
-
 int main(){
-	string s;
-	cin>>s;
-	int ans=LPS(s);
-	cout<<ans<<endl;
+	string s;cin>>s;
+    cout<<helper(s,s.size())<<endl;
+    return 0;
 }
