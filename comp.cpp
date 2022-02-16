50da06bfe0a9db65dcea75b9d6ecacdb96595011
Sakil Mostak <skmahim71@gmail.com>
2022-02-16 20:14:53 +0530

    Robbery Everywhere(Hackerrank)

--- comp.cpp
+++ comp.cpp
@@ -5,49 +5,81 @@
 #define endl "\n"
 using namespace std;
 
-int grundyOpt(ll n){
-	ll i=1LL;
-	ll j,k,l,m,res=0LL;
-	if(n==i){
-		return 1;
+bool UnionFindAlgo(int* parent,int n,int u,int v){
+	
+	int fparent=parent[u];
+	while(parent[fparent]!=fparent){
+		fparent=parent[fparent];
 	}
-	while(true){
+	int sparent=parent[v];
+	while(parent[sparent]!=sparent){
+		sparent=parent[sparent];
+	}
 
-		m=i+res;
-		if(n<=m){
-			return 1;
-		}
-		//cout<<m+1<<" ";
-		j=(2*i)+m;
-		if(n<=j){
-			return 2;
-		}
-		//cout<<j+1<<" ";
-		k=(2*i)+j;
-		if(n<=k){
-			return 3;
-		}
-		//cout<<k+1<<" ";
-		l=(6*i)+k;
-		if(n<=l){
-			return 0;
-		}
-		//cout<<l+1<<" ";
-		
-		res=l;
-		i=12*i;
+	if(fparent!=sparent){
+		int parentval=min(fparent,sparent);
+		parent[fparent]=parentval;
+		parent[sparent]=parentval;
+		return true;
 	}
+
+	return false;
+}
+
+class edge{
+	public:
+	int u,v,weight;
+};
+
+bool comp(edge a,edge b){
+	return a.weight<b.weight;
 }
 
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		ll n;
-		cin>>n;
-        
-		//cout<<grundy(n)<<endl;
-		cout<<grundyOpt(n)<<endl;
-       
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
 	}
+
+	int m;
+	cin>>m;
+	edge edges[m+n];
+	for(int i=0;i<m;i++){
+		cin>>edges[i].u;
+		cin>>edges[i].v;
+		int a,b;
+		cin>>a>>b;
+		edges[i].weight= a+b;
+	}
+
+	for(int i=m;i<m+n;i++){
+		edges[i].u=i-m;
+		edges[i].v=n;
+		edges[i].weight= arr[i-m];
+
+	}
+
+	sort(edges,edges+n+m,comp);
+
+	int* parent=new int[n+1];
+	for(int i=0;i<=n;i++){
+		parent[i]=i;
+	}
+
+	int count=0;
+	ll ans=0LL;
+	for(int i=0;i<n+m;i++){
+		if(UnionFindAlgo(parent,n+1,edges[i].u,edges[i].v)){
+			count++;
+			ans+=edges[i].weight;
+		}
+		if(count==n){
+			break;
+		}
+	}
+
+	cout<<ans<<endl;
 }
