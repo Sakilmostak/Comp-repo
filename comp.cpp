f364279b718fdb65dc7121d600888591378c0dad
Sakil Mostak <skmahim71@gmail.com>
2022-03-05 22:49:25 +0530

    Median of Two sorted array( O(log(min(n,m))))

--- comp.cpp
+++ comp.cpp
@@ -5,24 +5,54 @@
 #define endl "\n"
 using namespace std;
 
-double champagneTower(int poured, int query_row, int query_glass) {
-        float dp[100][100]={0};
-        dp[0][0]=poured/1.0;
-        for(int i=0;i<100;i++){
-            for(int j=0;j<=i;j++){
-                if(dp[i][j]>1){
-                    dp[i+1][j]+=(dp[i][j]-1)/2;
-                    dp[i+1][j+1]+=(dp[i][j]-1)/2;
-                    dp[i][j]=1.0;
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n= nums1.size();
+        int m=nums2.size();
+        if(n>m){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+
+        int start=0;
+        int end=n;
+        while(start<=end){
+            int mid= (start+end)/2;
+            int elemrem= (n+m+1)/2 - mid;
+
+            int l1= (mid==0)? INT_MIN: nums1[mid-1];
+            int l2= (elemrem==0)? INT_MIN: nums2[elemrem-1];
+
+            int r1= (mid==n)? INT_MAX: nums1[mid];
+            int r2= (elemrem==m)? INT_MAX: nums2[elemrem];
+
+            if(l1<=r2 && l2<=r1){
+                if((n+m)%2==0){
+                return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+                else{
+                    return max(l1,l2);
                 }
             }
+            else if(l1>r2){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
         }
-        
-        return dp[query_row][query_glass];
-        
-    }
 
+        return 0.0;
+}
 
 int main(){
-    cout<<champagneTower(6,3,2);
+    int n,m;
+    cin>>n>>m;
+    vector<int> v1(n),v2(m);
+    for(int i=0;i<n;i++){
+        cin>>v1[i];
+    }
+    for(int i=0;i<m;i++){
+        cin>>v2[i];
+    }
+
+    cout<<findMedianSortedArrays(v1,v2);
 }
