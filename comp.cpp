39541497e92062c6d21b03feb9e6aa470c4a21a0
Sakil Mostak <skmahim71@gmail.com>
2022-03-09 22:11:34 +0530

    DCP codechef

--- comp.cpp
+++ comp.cpp
@@ -6,34 +6,24 @@
 using namespace std;
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        ll ans=0;
-        ll maxy=0,miny=mod;
-        for(int i=0;i<n;i++){
-            ll x;
-            cin>>x;
-            x=(x>0)?x: -x;
-            if(i%2==0){
-                ans+=x;
-                if(x<miny){
-                    miny=x;
-                }
-            }
-            else{
-                ans-=x;
-                if(x>maxy){
-                    maxy=x;
-                }
-            }
+    int n,m;
+    cin>>n>>m;
+    ll arr[n+1];
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<m;i++){
+        int c,x;
+        cin>>c>>x;
+        for(int i=0;i<x;i++){
+            int wi,civ;
+            cin>>wi>>civ;
+            arr[civ]= ((arr[civ])%mod+ (wi*arr[c])%mod)%mod;
         }
-        
-        if(maxy>miny){
-            ans+=(2*(maxy-miny));
-        }
-        cout<<ans<<endl;
+        arr[c]=0;
+    }
+
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<endl;
     }
 }
