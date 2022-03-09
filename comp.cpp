5904b8e42df874a781b77253321e89e2a96bd895
Sakil Mostak <skmahim71@gmail.com>
2022-03-09 20:21:21 +0530

    codechef strp

--- comp.cpp
+++ comp.cpp
@@ -5,46 +5,26 @@
 #define endl "\n"
 using namespace std;
 
-int myAtoi(string s) {
-        while(s[0]==' '){
-            s=s.substr(1);
-        }
-        long sign=1;
-        if(s[0]=='+'){
-            sign=1;
-        }
-        else if(s[0]=='-'){
-            sign=-1;
-        }
-        
-        if(s[0]=='+' || s[0]=='-'){
-            s=s.substr(1);
-        }
-        
-        long ans=0L;
-        
-        for(const char c: s)
-        {
-            if(c>='0' && c<='9'){
-                int digit= c-'0';
-                ans=(10*ans)+digit;
-                
-                if(sign*ans<INT_MIN){
-                    return INT_MIN;
-                }
-                if(sign*ans>INT_MAX){
-                    return INT_MAX;
-                }
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0;i<n;){
+            if(s[i]==s[i+1] && i!=n-1){
+                count++;
+                i+=2;
             }
             else{
-                return sign*ans;
+                count++;
+                i++;
             }
-        } 
-        
-        return sign*ans;
+        }
+
+        cout<<count<<endl;
     }
-
-
-int main(){
-	cout<<myAtoi("-927419734927298292834");
 }
