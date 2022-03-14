c42d656399e5b257b65d9f060c1176c08bf2dcf9
Sakil Mostak <skmahim71@gmail.com>
2022-03-14 12:36:45 +0530

    Maximize the topmost element after K moves leetcode

--- comp.cpp
+++ comp.cpp
@@ -7,37 +7,38 @@ using namespace std;
 
 class Solution {
 public:
-    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
-        bool arr[1000][1000]={0};
-        for(int i=0;i<dig.size();i++){
-            arr[dig[i][0]][dig[i][1]]=1;
+    int maximumTop(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(k==0){
+            return nums[0];
         }
         
-        int n1= artifacts.size();
-        
-        int count=0;
-        
-        for(int f=0;f<n1;f++){
-
-            int x1= artifacts[f][0];
-            int y1= artifacts[f][1];
-            int x2= artifacts[f][2];
-            int y2= artifacts[f][3];
-            //cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
-            bool flag=true;
-            for(int j=x1;j<=x2;j++){
-                for(int k=y1;k<=y2;k++){
-                    if(!arr[j][k]){
-                        flag=false;
-                        break;
-                    }
-                }
+        if(k==1){
+            if(n<=1){
+                return -1;
             }
-            if(flag){
-                count++;
+            else{
+                return nums[1];
             }
         }
-        return count;
         
+        if(n==1){
+            if(k%2==0){
+                return nums[0];
+            }
+            
+            return -1;
+        }
+        
+        int maxele=INT_MIN;
+        for(int i=0;i<min(k-1,n);i++){
+            maxele= max(maxele, nums[i]);
+        }
+        
+        if(k<n){
+            return max(maxele, nums[k]);
+        }
+        
+        return maxele;
     }
 };
