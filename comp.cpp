d5d66db6471e62a9eaa47366db22cb682554712a
Sakil Mostak <skmahim71@gmail.com>
2022-03-20 09:32:19 +0530

    Count Hill and Valleys in an Array(Leetcode contest)

--- comp.cpp
+++ comp.cpp
@@ -5,57 +5,68 @@
 #define endl "\n"
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-        } 
-
-        // finding LIS till that index
-
-        vector<int> lis, prefix(n);
-        for(int i=0;i<n;i++){
-            auto it=lower_bound(lis.begin(),lis.end(),v[i])-lis.begin();
-            if(it==lis.size()){
-                lis.push_back(v[i]);
+int countHillValley(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int i=1;
+        while(i<n-1){
+            int j=i-1;
+            bool lh=0,rh=0;
+            while(nums[i]==nums[j] && j>0){
+                j--;
             }
-            else{
-                lis[it]= v[i];
+            
+            if(nums[i]==nums[j]){
+                i++;
+                continue;
             }
-
-            prefix[i]=lis.size();
+            else if(nums[j]!=nums[i]){
+                if(nums[j]>nums[i]){
+                    lh=0;
+                }
+                else{
+                    lh=1;
+                }
+            }
+            
+            
+            
+            j=i+1;
+            
+            while(nums[i]==nums[j] && j<n-1){
+                j++;
+            }
+            
+            if(nums[i]==nums[j]){
+                break;
+            }
+            else if(nums[i]!=nums[j]){
+                if(nums[i]<nums[j]){
+                    rh=0;
+                }
+                else{
+                    rh=1;
+                }
+            } 
+            
+            if(!(lh^rh)){
+                count++;
+            }
+            
+            i=j;
         }
-
-        lis.clear();
-
-        // finding LIS possible after that index
-
-        vector<int> suffix(n);
-        for(int i=n-1;i>=0;i--){
-            auto it= lower_bound(lis.begin(),lis.end(),-v[i])-lis.begin();
-            if(it==lis.size()){
-                lis.push_back(-v[i]);
-            }
-            else{
-                lis[it]=-v[i];
-            }
-            suffix[i]=lis.size();
-        }
-
-        //LIS possible by performing operation in an subarray at that index is prefix[i-1]+suffix[i]
-
-        int ans=suffix[0];
-
-        for(int i=1;i<n;i++){
-            ans=max(ans, prefix[i-1]+suffix[i]);
-        }
-
-        cout<<ans<<endl;
         
+        return count;
     }
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<countHillValley(arr);
+        
 }
